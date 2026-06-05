07b45220740d554939e0a9d4d75287c38c305407
DoubleG2s <gui.mail1@proton.me>
2026-06-05 14:09:24 -0300

    chore: atualiza precos locais de teste para 5 reais

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   geminiApiKey: '',
   stripePublishableKey: 'pk_live_51TILDLP0RV1HtDUPbba8XgpWFZ7SzAAcVoF4tpAsBhHkP3WX09GyjmO29wqfG6YWW7Alu9ylP26htaNoW2YQI7vx00cV5mAT1c',
   apiBaseUrl: '',
-  monthlyPrice: 370
+  monthlyPrice: 5
 };
