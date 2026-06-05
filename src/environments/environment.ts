e5893925002de4f7169d3c18ddede53f3b9b3165
DoubleG2s <gui.mail1@proton.me>
2026-06-05 14:11:01 -0300

    chore: restaura preco oficial de 370 reais na assinatura

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   geminiApiKey: '',
   stripePublishableKey: 'pk_test_51TILDYP2xn69ECMDP2ViDRmFjUVFJLsd40HRo2pvPPzytYfMIm7xDqPhhU9iiA8vt4KlBcpR6D49eovnd77p5Wa100O6uc2NUC',
   apiBaseUrl: '',
-  monthlyPrice: 5
+  monthlyPrice: 370
 };
